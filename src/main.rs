6912c927680bedbf42f5f01ffa8b1711cfa1e91b
Jessy <jessy_drouin@protonmail.com>
2023-03-02 13:48:43 +0100

    Return an array of CPUs as Json

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,41 @@
 use std::sync::{Arc, Mutex};
 
-use axum::{extract::State, routing::get, Router, Server};
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router, Server};
 use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new()
-        .route("/", get(root_get))
+    let router = Router::new() // Creating the router. It is a tree of routes. Each route is a function that will be called when the route is matched.
+        .route("/", get(root_get)) // Adding a route. The first argument is the path. The second argument is the function that will be called when the route is matched.
+        .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
-            sys: Arc::new(Mutex::new(System::new())),
+            // Adding the state to the router. This state will be available to all routes. It is wrapped in an Arc to allow it to be shared between threads.
+            sys: Arc::new(Mutex::new(System::new())), // Creating a new System information structure. It is wrapped in a Mutex to allow it to be shared between threads.
         });
-    let server = Server::bind(&"0.0.0.0:8081".parse().unwrap()).serve(router.into_make_service());
-    let addr = server.local_addr();
-    println!(" Listing on {addr}");
+    let server = Server::bind(&"0.0.0.0:8081".parse().unwrap()).serve(router.into_make_service()); // Starting the server.
+    let addr = server.local_addr(); // Getting the address on which the server is listening.
+    println!(" Listing on {addr}"); // Printing the address on which the server is listening.
 
-    server.await.unwrap();
+    server.await.unwrap(); // Waiting for the server to stop.
 }
 
 #[derive(Clone)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    sys: Arc<Mutex<System>>, // System information structure. It is wrapped in a Mutex to allow it to be shared between threads.
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
-    use std::fmt::Write;
+async fn root_get() -> &'static str {
+    // This function will be called when the route "/" is matched.
+    "Yosh ! "
+}
 
-    let mut s = String::new();
-    let mut sys = state.sys.lock().unwrap();
-
+#[axum::debug_handler]
+async fn cpus_get(State(state): State<AppState>) -> impl IntoResponse {
+    let mut sys = state.sys.lock().unwrap(); // Getting the System information structure from the state. It is wrapped in a Mutex to allow it to be shared between threads.
+                                             // First we update all information of our `System` struct.
     sys.refresh_cpu(); // Refreshing CPU information.
 
-    for (i, cpu) in sys.cpus().iter().enumerate() {
-        let i = i + 1;
-        let usage = cpu.cpu_usage();
+    let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect(); // Getting the CPU usage. It is a value between 0 and 100.
 
-        writeln!(&mut s, "CPU {i} {usage}%").unwrap();
-    }
-
-    s
+    Json(v)
 }
