d4dd6c5e246818fd2c102485a3287c9e908412d9
Jessy <jessy_drouin@protonmail.com>
2023-03-03 10:36:33 +0100

    Add preact

---
+++ src/index.mjs
@@ -0,0 +1,20 @@
+import { h, Component, render } from "https://unpkg.com/preact?module";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let i = 0;
+
+  setInterval(async () => {
+    let response = await fetch("/api/cpus");
+    if (response.status !== 200) {
+      throw new Error(`Failed to fetch status: ${response.status}`);
+    }
+
+    let json = await response.json();
+
+    i = i + 1;
+    // document.body.textContent = JSON.stringify(json, null, 2);
+    const app = h("pre", null, JSON.stringify(json, null, 2));
+
+    render(app, document.body);
+  }, 1000);
+});
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use sysinfo::{CpuExt, System, SystemExt};
 async fn main() {
     let router = Router::new() // Creating the router. It is a tree of routes. Each route is a function that will be called when the route is matched.
         .route("/", get(root_get)) // Adding a route. The first argument is the path. The second argument is the function that will be called when the route is matched.
-        .route("/index.js", get(indexjs_get))
+        .route("/index.mjs", get(indexmjs_get))
         .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
             // Adding the state to the router. This state will be available to all routes. It is wrapped in an Arc to allow it to be shared between threads.
@@ -39,8 +39,8 @@ async fn root_get() -> impl IntoResponse {
 }
 
 #[axum::debug_handler]
-async fn indexjs_get() -> impl IntoResponse {
-    let markup = tokio::fs::read_to_string("src/index.js").await.unwrap(); // Reading the index.js file.
+async fn indexmjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.mjs").await.unwrap(); // Reading the index.js file.
 
     Response::builder() // Creating the response.
         .header("content-type", "application/javascript;charset=utf-8")
